a9978ed0e9a43dc962dbdaac3f32e9eef814a85a
unknown <aananthv@outlook.com>
2022-04-20 13:27:00 +0530

    feat: Try to make lines thicker

--- viz/src/renderBVH.ts
+++ viz/src/renderBVH.ts
@@ -1,8 +1,7 @@
-import { AmbientLight, AnimationClip, AnimationMixer, Bone, BoxGeometry, Clock, Color, GridHelper, Group, KeyframeTrack, Mesh, MeshStandardMaterial, PerspectiveCamera, Plane, PlaneGeometry, PlaneHelper, Scene, Skeleton, SkeletonHelper, Vector3, VectorKeyframeTrack, WebGLRenderer } from 'three';
-import { BVH, BVHLoader } from 'three/examples/jsm/loaders/BVHLoader';
+import { AmbientLight, AnimationClip, AnimationMixer, Bone, BoxGeometry, Clock, Color, CylinderGeometry, GridHelper, Group, KeyframeTrack, LineBasicMaterial, Mesh, MeshStandardMaterial, PerspectiveCamera, Plane, PlaneGeometry, PlaneHelper, Scene, Skeleton, SkeletonHelper, SkinnedMesh, Vector3, VectorKeyframeTrack, WebGLRenderer } from 'three';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 
 export default class RenderBVH {
-    loader: BVHLoader;
     renderer: WebGLRenderer;
     skeletonHelper: SkeletonHelper;
     scene: Scene;
@@ -10,6 +9,7 @@ export default class RenderBVH {
     camera: PerspectiveCamera;
     clock: Clock;
     id: number;
+    skeleton_meshes: Mesh[];
 
     constructor(canvas: HTMLCanvasElement, motionSequence: number[][][], clock: Clock, id: number) {
         this.clock = clock;
@@ -18,65 +18,79 @@ export default class RenderBVH {
         this.init(canvas);
         this.animate();
 
-        // this.loader.load(bvhFile, (result: BVH) => {
-        //     console.log(result);
-
-        //     this.skeletonHelper = new SkeletonHelper(result.skeleton.bones[0]);
-
-        //     // @ts-ignore
-        //     this.skeletonHelper.skeleton = result.skeleton
-
-        //     this.scene.add(this.skeletonHelper);
-
-        //     const boneContainer = new Group();
-        //     boneContainer.add(result.skeleton.bones[0]);
-        //     this.scene.add(boneContainer);
-
-        //     this.mixer = new AnimationMixer(this.skeletonHelper);
-            
-        //     const animationClip = new AnimationClip(
-        //         'animation',
-        //         -1,
-        //         result.clip.tracks.map(track => {
-        //             const values = []
-        //             for (let i = 0; i < track.values.length; i += 3) {
-        //                 values.push(
-        //                     track.values[i],
-        //                     track.values[i + 1],
-        //                     track.values[i + 2]
-        //                 )
-        //             }
-
-        //             const times = [...Array(track.times.length)].map((_, i) => i / 30);
-
-        //             return new VectorKeyframeTrack(
-        //                 track.name,
-        //                 times,
-        //                 values
-        //             )
-        //         })
-        //     )
-
-        //     console.log(animationClip)
-
-        //     console.log(result.clip)
-
-        //     this.mixer.clipAction(animationClip).play();
-        // })
-
         const [skeleton, animationClip] = this.constructSkeleton(motionSequence);
 
         this.skeletonHelper = new SkeletonHelper(skeleton.bones[0]);
         // @ts-ignore
         this.skeletonHelper.skeleton = skeleton
 
+        if (this.skeletonHelper.material instanceof LineBasicMaterial) {
+            this.skeletonHelper.material.linewidth = 10
+        }
+
         this.scene.add(this.skeletonHelper);
 
         const boneContainer = new Group();
         boneContainer.add(skeleton.bones[0]);
+
         this.scene.add(boneContainer);
 
         this.mixer = new AnimationMixer(this.skeletonHelper);
+
+        this.skeleton_meshes = [];
+
+        // skeleton.bones.forEach((bone, index) => {
+        //     if (!(bone.parent instanceof Bone)) return;
+
+        //     const height = bone.position.distanceTo(bone.parent.position);
+        //     const geometry = new CylinderGeometry(1, 1, height);
+        //     const material = new MeshStandardMaterial();
+
+        //     const mesh = new Mesh(geometry, material);
+        //     setMesh(mesh, bone);
+
+        //     this.skeleton_meshes.push(mesh);
+
+        //     this.scene.add(mesh);
+        // })
+
+        // const loader = new FBXLoader();
+        // loader.load('./static/skeleton_fucked.fbx', model => {
+        //     model.traverse(child => {
+        //         if (child instanceof SkinnedMesh) {
+        //             // console.log("Original FBX Bones")
+        //             // child.skeleton.bones.forEach(bone => console.log(bone.name, bone.parent.name))
+
+        //             // console.log("Original BVH Bones")
+        //             // skeleton.bones.forEach(bone => console.log(bone.name, bone.parent.name))
+
+        //             skeleton.pose()
+
+        //             child.skeleton.bones = skeleton.bones.map(
+        //                 (_, index) => {
+        //                     // console.log(skeleton.bones, index, skeleton.bones[index])
+        //                     return child.skeleton.getBoneByName(skeleton.bones[index].name)
+        //                 }
+        //             );
+
+        //             console.log(child.skeleton.bones[0])
+        //             console.log(skeleton.bones[0])
+
+        //             // child.skeleton.bones = child.skeleton.bones.map((_, index) => skeleton.bones[permutation[index]]);
+
+        //             // console.log("Permuted FBX Bones")
+        //             // child.skeleton.bones.forEach(bone => console.log(bone.name, bone.parent.name))
+
+        //             // skeleton.pose();
+        //             child.bind(skeleton);
+        //             // child.add(skeleton.bones[0])
+        //         }
+        //     });
+
+        //     this.scene.add(model);
+        // })
+
+
         this.mixer.clipAction(animationClip).play();
     }
 
@@ -91,7 +105,7 @@ export default class RenderBVH {
         this.scene.add(new AmbientLight());
 
         const ground = new Mesh(
-            new PlaneGeometry(300, 300), 
+            new PlaneGeometry(300, 300),
             new MeshStandardMaterial({ color: 0x333333 })
         );
         ground.rotation.set(-Math.PI / 2, 0, 0);
@@ -102,8 +116,6 @@ export default class RenderBVH {
         this.renderer = new WebGLRenderer({ antialias: true, canvas: canvas });
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-
-        this.loader = new BVHLoader();
     }
 
     resizeCanvasToDisplaySize() {
@@ -111,15 +123,15 @@ export default class RenderBVH {
         // look up the size the canvas is being displayed
         const width = canvas.clientWidth;
         const height = canvas.clientHeight;
-      
+
         // adjust displayBuffer size to match
         if (canvas.width !== width || canvas.height !== height) {
-          // you must pass false here or three.js sadly fights the browser
-          this.renderer.setSize(width, height, false);
-          this.camera.aspect = width / height;
-          this.camera.updateProjectionMatrix();
+            // you must pass false here or three.js sadly fights the browser
+            this.renderer.setSize(width, height, false);
+            this.camera.aspect = width / height;
+            this.camera.updateProjectionMatrix();
         }
-      }
+    }
 
     animate() {
         requestAnimationFrame(() => this.animate());
@@ -129,6 +141,11 @@ export default class RenderBVH {
         const delta = this.clock.getDelta();
 
         if (this.mixer) this.mixer.update(delta);
+
+        if (this.skeleton_meshes) this.skeleton_meshes.forEach((mesh, index) => {
+            const bone = this.skeletonHelper.bones[index];
+            setMesh(mesh, bone);
+        })
 
         this.renderer.render(this.scene, this.camera);
     }
@@ -147,7 +164,7 @@ export default class RenderBVH {
 
         parents.forEach((parent, index) => {
             const bone = new Bone();
-            bone.name = `bone${index}`;
+            bone.name = `${names[index]}`;
             bone.position.fromArray(x[index]);
             bones.push(bone);
 
@@ -171,18 +188,18 @@ export default class RenderBVH {
         })
 
         const times = [...Array(tracks[0].length)].map((_, i) => i * 0.033);
-        
+
         const keyframeTracks = tracks.map((jointPositions, index) => {
             const vector3Sequence = jointPositions.flatMap(
                 p => p
             )
 
             const vectorTrack = new VectorKeyframeTrack(
-                `.bones[bone${index}].position`,
+                `.bones[${names[index]}].position`,
                 times,
                 vector3Sequence
             )
-            
+
             return vectorTrack
         })
 
@@ -192,4 +209,35 @@ export default class RenderBVH {
     }
 }
 
+const setMesh = (mesh: Mesh, bone: Bone) => {
+    mesh.position.fromArray(bone.position.toArray());
+    mesh.rotation.setFromVector3(bone.parent.position.clone().sub(bone.position).normalize());
+}
+
 const parents = [-1, 0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 12, 11, 14, 15, 16, 11, 18, 19, 20];
+const permutation = [0, 9, 10, 11, 12, 13, 18, 19, 20, 21, 14, 15, 16, 17, 5, 6, 7, 8, 1, 2, 3, 4];
+// const permutation = [0, 18, 19, 20, 21, 14, 15, 16, 17, 1, 2, 3, 4, 5, 10, 11, 12, 13, 6, 7, 8, 9];
+const names = [
+    "ModelHips",
+    "ModelLeftUpLeg",
+    "ModelLeftLeg",
+    "ModelLeftFoot",
+    "ModelLeftToe",
+    "ModelRightUpLeg",
+    "ModelRightLeg",
+    "ModelRightFoot",
+    "ModelRightToe",
+    "ModelSpine",
+    "ModelSpine1",
+    "ModelSpine2",
+    "ModelNeck",
+    "ModelHead",
+    "ModelLeftShoulder",
+    "ModelLeftArm",
+    "ModelLeftForeArm",
+    "ModelLeftHand",
+    "ModelRightShoulder",
+    "ModelRightArm",
+    "ModelRightForeArm",
+    "ModelRightHand",
+]
